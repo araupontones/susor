0c857de11f0a53c22d7d8755a6808c9fe595d46c
araupontones <43657130+araupontones@users.noreply.github.com>
2021-05-24 18:32:10 +0200

    add dplyr:: to filters

--- R/susor_get_qnID.R
+++ R/susor_get_qnID.R
@@ -1,11 +1,11 @@
- 
+
 #' FUNCTION TO GET QN ID
 
 susor_get_qnID = function(susor_qn_variable,
                           susor_qn_version,
                        ...){
 
-      quid = susor_questionnaires %>% filter(Variable == susor_qn_variable & Version == susor_qn_version) %>% .$QuestionnaireIdentity
+      quid = susor_questionnaires %>% dplyr::filter(Variable == susor_qn_variable & Version == susor_qn_version) %>% .$QuestionnaireIdentity
 
       return(quid)
 
